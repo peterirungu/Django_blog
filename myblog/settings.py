97a8416df7a526b8ccb57ff81f25cad7b320beb9
peterirungu <peterirungu16@gmail.com>
2022-03-08 12:21:27 +0300

    django heroku configuration

--- myblog/settings.py
+++ myblog/settings.py
@@ -119,9 +119,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+
+
+
 STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+import django_heroku
+django_heroku.settings(local())
